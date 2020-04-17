90bd884b6ed541bf92038c7de3153e33ce350341
AhmedAdly20 <ahmedadly132@gmail.com>
2020-04-17 21:23:22 +0200

    VUE-API

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index";
+import store from "./store/index";
 
 Vue.config.productionTip = false;
+
+import VueSpinners from "vue-spinners";
+
+Vue.use(VueSpinners);
 
 new Vue({
   router,
